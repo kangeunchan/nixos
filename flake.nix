a8df050a20cf2dc6c5074a6bf6abde7cbba342b0
kangeunchan <kangeunchan080310@gmail.com>
2026-01-29 17:18:24 +0900

    feat: disko 모듈 구성 개선 및 flake-utils 제거

--- flake.nix
+++ flake.nix
@@ -3,25 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    flake-utils.url = "github:numtide/flake-utils";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, flake-utils, disko, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        cfgName = "rkddmscks-${system}";
-      in
-      {
-        nixosConfigurations.${cfgName} = pkgs.nixosSystem {
-          inherit system;
-          modules = [
-            disko.nixosModules.disko
-            ./modules/systems/disks.nix
-            ./modules/configurations/configuration.nix
-          ];
-        };
-      });
+  outputs = { nixpkgs, disko, ... }:
+  {
+    nixosConfigurations.rkddmscks-aarch64-linux = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      modules = [
+        disko.nixosModules.disko
+        ./modules/systems/disks.nix
+        ./modules/configurations/configuration.nix
+      ];
+    };
+
+    nixosConfigurations.rkddmscks-x86_64-linux = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        disko.nixosModules.disko
+        ./modules/systems/disks.nix
+        ./modules/configurations/configuration.nix
+      ];
+    };
+  };
 }
